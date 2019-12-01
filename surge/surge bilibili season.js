0614f1c49e7fc703391e29bcf1c4c139b11abbca
onewayticket255 <15365182027@163.com>
2019-12-01 22:03:46 +0800

    fix bug

--- surge/surge bilibili season.js
+++ surge/surge bilibili season.js
@@ -6,20 +6,27 @@ let url = $request.url
 function getParams(key) {
     let regex = new RegExp(`${key}=(\\d*?)&`)
     let tmp = regex.exec(url)
-    return tmp[1]
+    if (tmp) {
+        return tmp[1]
+    }
 }
 
 let season_id = getParams("season_id")
 
-let api = `https://bilibili.mlyx.workers.dev/?season_id=${season_id}`
+if (season_id) {
 
-$httpClient.get(api, (error, response, body) => {
-    if (error || response.status == 404) {
-        $notification.post('获取番剧信息失败', '使用原始信息', 'biliplus未收录此资源或服务器错误')
-        $done({})
-    }
-    else {
-        $notification.post('获取番剧信息成功', '使用大会员信息', 'success')
-        $done({ body })
-    }
-})
+    let api = `https://bilibili.mlyx.workers.dev/?season_id=${season_id}`
+
+    $httpClient.get(api, (error, response, body) => {
+        if (error || response.status == 404) {
+            $notification.post('获取番剧信息失败', '使用原始信息', 'biliplus未收录此资源或服务器错误')
+            $done({})
+        }
+        else {
+            $notification.post('获取番剧信息成功', '使用大会员信息', 'success')
+            $done({ body })
+        }
+    })
+} else {
+    $done({})
+}
