84f8892831e3a77f89f0ab2f353b9c644c827392
onewayticket255 <15365182027@163.com>
2019-12-28 22:40:45 +0800

    simplify

--- server/bilibili/model/sign.js
+++ server/bilibili/model/sign.js
@@ -1,9 +1,9 @@
 const config = require('../config.js')
 const crypto = require('crypto')
-const qs = require('qs')
+const querystring = require('querystring')
 
 const baseSign = params => crypto.createHash('md5').update(`${params}${config.BASE_SECRET}`).digest('hex')
 const videoSign = params => crypto.createHash('md5').update(`${params}${config.VIDEO_SECRET}`).digest('hex')
-const makeParams = (params, sign) => `${qs.stringify(params)}&sign=${sign(qs.stringify(params))}`
+const makeParams = (params, sign) => `${querystring.stringify(params)}&sign=${sign(querystring.stringify(params))}`
 
 module.exports = { baseSign, videoSign, makeParams }
