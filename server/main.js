beb6d83cd8212e3dd53f79cc8d0cc449db63657d
onewayticket255 <15365182027@163.com>
2020-02-09 19:59:41 +0800

    update

--- server/main.js
+++ server/main.js
@@ -1,12 +1,12 @@
 const http = require('http')
 const { net_getAccessKey, net_season, net_playurl } = require('./bilibili/network.js')
 
-let accessKey = ''
-
 const unixtime = () => Math.round(Date.now() / 1000)
 //For Debug
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 
+/* Deprecated login, TODO: captcha recognition
+let accessKey = ''
 const init = async () => {
     accessKey = await net_getAccessKey(unixtime())
     console.log(accessKey)
@@ -14,8 +14,13 @@ const init = async () => {
         accessKey = await net_getAccessKey(unixtime())
     }, 2100000000) //大概20天登录一次
 }
-
 init()
+*/
+
+
+//Capture accessKey from iOS app directly
+//Expires in 30 days
+const accessKey= ''
 
 
 const HttpServer = new http.Server()
